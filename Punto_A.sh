3a5afb8b30281f28bf12f1a74e6acb46f3df300a
rangelyusmelis <ryusmeis23@gmail.com>
2024-12-01 19:52:19 +0000

    actualizacion Punto A

--- Punto_A.sh
+++ Punto_A.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
+
+sudo fdisk -l
+
+#particionamos los discos
+sudo fdisk /dev/sdc
+
 sudo fdisk /dev/sdd
 lsblk
 free -h
+#limpiamos las particiones
 sudo wipefs -a /dev/sdc1 /dev/sdd1 /dev/sdd2
 cd
 lsblk
+#creo los volumenes fisicos
 sudo pvcreate /dev/sdc1 /dev/sdd1 /dev/sdd2
 sudo pvs
+
+#creo los grupos
 sudo vgcreate vg_datos /dev/sdc1
 sudo vgcreate vg_datos /dev/sdd1 /dev/sdd2
 sudo vgcreate vg_temp /dev/sdd1 /dev/sdd2
 sudo pvs
 sudo vgs
+
+#creo los volumnes logicos:
 sudo lvcreate -L 5M vg_datos -n lv_docker
 sudo lvcreate -L 1.5G vg_datos -n lv_workareas
 sudo lvcreate -L 512M vg_temp -n lv_swap
 sudo lvs
 sudo vgs
 sudo fdisk -l
-sudo mkfs Disk /dev/mapper/vg_temp-lv_swap
+
+#doy formato para la memoria swap
+
 sudo mkswap /dev/mapper/vg_temp-lv_swap
+
+#la monto
 sudo swapon /dev/mapper/vg_temp-lv_swap
 swapon -s
 free -h
+
+#doy formato a los volumenes logicos:
 sudo mkfs -t ext4 /dev/mapper/vg_datos-lv_docker
-slludo mkfs -t ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkfs -t ext4 /dev/mapper/vg_datos-lv_workareas
 sudo lsblk -f
 ls -l /var/lib/docker
 
@@ -36,6 +54,7 @@ ls - a
 cd lib
 ls -la
 
+#monto los volumenes:
 lsblk
 sudo fdisk -l
 sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker
@@ -47,6 +66,8 @@ df -h
 cat /etc/fstab
 sudo mount /home/vagrant/foo
 mount | grep foo
+
+#hago el montaje persistente
 cat /etc/fstab
 lsblk
 echo "/dev/vg_datos/lv_docker /var/lib/docker ext4 defaults 0 0" | sudo tee -a /etc/fstab
